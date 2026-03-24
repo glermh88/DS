fa155edadaf7912dfed4b2683cc36ba4a281899f
glermh88 <yoriichisoladorhaha@gmail.com>
2026-03-24 20:50:28 -0300

    Add files via upload

---
+++ CalculoMedia.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+public class CalculoMedia {
+
+    public static void main(String[] args) {
+        String nomeDoAluno;
+double nota1;
+double nota2;
+double nota3;
+double nota4;
+    
+         nomeDoAluno = "joao";
+         
+         nota1 = 5.5;
+         nota2 = 6.6;
+         nota3 = 7.7;
+         nota4= 8.8;
+         
+         double media = (nota1 + nota2 + nota3 + nota4) /4;
+         
+         System.out.println("Aluno " + nomeDoAluno + ": Media " + media);
+     
+    }
+    
+}
---
+++ Programadores.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programadores;
+
+import java.util.Scanner;
+
+public class Programadores {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+ String[] frutas = new String[5];
+ frutas[0] = "banana ";
+ frutas[1] = "maçã ";
+ frutas [2] = "goiaba ";
+ frutas [3] = "uva ";
+ frutas [4] = "abacaxi ";
+    
+    System.out.println("Programadores, avante! " + frutas[0] + frutas[1] + frutas[2] + frutas [3] + frutas [4]);
+}
+}
